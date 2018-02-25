42dab9d494d90768369b1702aa6e790479b817d0
lsankar4033 <lsankar4033@gmail.com>
2018-02-25 01:03:00 -0800

    A whole lot of initialization stuff to get client set up

--- contracts/BirthdayCoin.sol
+++ contracts/BirthdayCoin.sol
@@ -2,11 +2,10 @@ pragma solidity ^0.4.4;
 
 // import './NFT.sol';
 
-// TODO: Consider making this thing follow ERC721
+// TODO: Consider making this thing abide by ERC721
 contract BirthdayCoin  {
 
   // TODO: Refine
-  string constant defaultMessage = "For Sale!";
   uint constant startingPrice = 1 finney;
 
   // There are 366 coins:
@@ -19,28 +18,34 @@ contract BirthdayCoin  {
   // 2/29 | 365 (leap day)
   mapping(uint => address) public coinToOwner;
   mapping(uint => string) public coinToMessage;
-  mapping(uint => uint) public coinToPrice;
+  mapping(uint => uint) public coinToPrice; // TODO: for clarity, maybe this should reflect the initial price too..
   mapping(address => uint) _pendingWithdrawals;
 
   // May not need this
-  address public creator = msg.sender;
+  address public creator;
 
-  // Initialize all coins to be owned by creator with some default message
-  // TODO: In truffle, this is running out of gas...
   function BirthdayCoin() public {
-    for (uint coin = 0; coin < 366; coin++) {
-      coinToOwner[coin] = creator;
-      coinToMessage[coin] = defaultMessage;
-      coinToPrice[coin] = startingPrice;
-    }
+    creator = msg.sender;
   }
 
+  // TODO: (maybe) add fee to creator
   function buyBirthday(uint id, string message) public payable returns (bool) {
     require(id >= 0 && id < 366);
 
-    // TODO: determine how fee to creator (and gas) works
-    if (msg.value > coinToPrice[id]) {
-      _pendingWithdrawals[coinToOwner[id]] += msg.value;
+    // May be cleaner to structure price/owner/message as struct and condition creator check below on
+    // whether that struct is defined
+    uint price;
+    address owner;
+    if (coinToPrice[id] == 0) {
+      price = startingPrice;
+      owner = creator;
+    } else {
+      price = coinToPrice[id];
+      owner = coinToOwner[id];
+    }
+
+    if (msg.value > price) {
+      _pendingWithdrawals[owner] += msg.value;
       coinToOwner[id] = msg.sender;
       coinToMessage[id] = message;
       coinToPrice[id] = _determineNewPrice(msg.value);
@@ -59,5 +64,4 @@ contract BirthdayCoin  {
     _pendingWithdrawals[msg.sender] = 0; // zero out withdrawal first to protect against re-entrancy
     msg.sender.transfer(amount);
   }
-
 }
---
+++ src/js/app.js
@@ -0,0 +1,50 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: function() {
+    // Initialization logic?
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // Is there an injected web3 instance?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fall back to Ganache
+      App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON('BirthdayCoin.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var BirthdayCoinArtifact= data;
+      App.contracts.BirthdayCoin = TruffleContract(BirthdayCoinArtifact);
+
+      // Set the provider for our contract
+      App.contracts.BirthdayCoin.setProvider(App.web3Provider);
+
+      // TODO: any logic to mark all known birthdays
+    });
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    // TODO
+  },
+
+  // TODO: binding for buying a birthday or withdrawing balance
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
