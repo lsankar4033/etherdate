74c8492890ec968719fe74b9bde52cd0161e9921
lsankar4033 <lsankar4033@gmail.com>
2018-03-01 21:26:25 -0800

    Make BirthdayCoin.json accessible to http server

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # TODO
 - deploy webapp
+  - make sure prod can load BirthdayCoin.json
+    - (right now only lite-server works properly)
+  - add prod http-server setup to package.json (and readme?)
 - (optional) configurability of which net to connect to
 - (optional) open source code?
   - instead could just include the ABI interface or something on the site
--- src/js/app.js
+++ src/js/app.js
@@ -65,7 +65,8 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON('BirthdayCoin.json', function(data) {
+    // NOTE: This is the version of BirthdayCoin.json that is *live* on the mainnet
+    $.getJSON('resources/BirthdayCoin.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with truffle-contract
       const abstractContract = TruffleContract(data);
       abstractContract.setProvider(App.web3Provider);
